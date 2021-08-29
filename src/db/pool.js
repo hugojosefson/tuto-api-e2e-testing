19cdf4492661d286ca07ad72b6cacb2469a462a1
Hugo Josefson <hugo@josefson.org>
2021-08-29 17:44:55 +0000

    Always wait until server is up.

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,49 +1,38 @@
 import { createPool } from 'mysql2/promise.js'
 
-export async function getPool (dbConfig) {
-  return await createPool({
-    ...dbConfig,
-    namedPlaceholders: true,
-    multipleStatements: true
-  })
-}
-
 async function sleep (seconds) {
   return new Promise((resolve) => {
     setTimeout(resolve, seconds * 1000)
   })
 }
 
-async function tryConnect (dbConfig) {
-  let pool
-  try {
-    pool = await getPool(dbConfig)
-    await pool.execute('select 0')
-    return pool
-  } catch (error) {
-    if (error.code === 'ECONNREFUSED') {
-      console.warn('WARN Retrying, because pool is not ready yet.')
-    } else {
-      console.warn('WARN Retrying, because pool is not ready yet. Unexpected error: ', error)
+export async function getPool (dbConfig) {
+  while (true) {
+    let pool
+    try {
+      pool = await createPool({
+        ...dbConfig,
+        namedPlaceholders: true,
+        multipleStatements: true
+      })
+      await pool.execute('select 0')
+      return pool
+    } catch (error) {
+      if (error.code === 'ECONNREFUSED') {
+        console.warn('WARN Retrying, because pool is not ready yet.')
+      } else {
+        console.warn('WARN Retrying, because pool is not ready yet. Unexpected error: ', error)
+      }
+      if (pool && typeof pool.end === 'function') {
+        await pool.end()
+      }
+      await sleep(2)
     }
-    if (pool && typeof pool.end === 'function') {
-      await pool.end()
-    }
-    await sleep(2)
-    return 'try_again'
   }
-}
-
-async function eventuallyConnect (dbConfig) {
-  let pool = 'try_again'
-  while (pool === 'try_again') {
-    pool = await tryConnect(dbConfig)
-  }
-  return pool
 }
 
 export async function ensureDbServerIsUp (dbConfig) {
-  const pool = await eventuallyConnect(dbConfig)
+  const pool = await getPool(dbConfig)
   if (pool && typeof pool.end === 'function') {
     await Promise.resolve(pool.end())
     console.error('Ended the pool.')
