5084f3d46f4d47edcf72ca1bde3ca4fccae977cc
Hugo Josefson <hugo@josefson.org>
2021-08-29 16:40:23 +0200

    Use migrations.

---
+++ migrations/001-create-user-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE user (
+    PRIMARY   KEY (id),
+    id        CHAR(36)     NOT NULL,
+    email     VARCHAR(256) NOT NULL,
+    firstname VARCHAR(256) NOT NULL
+);
--- src/db/init.js
+++ src/db/init.js
@@ -1,33 +1,12 @@
 #!/usr/bin/env node
 
+import { resolve } from 'path'
+import marv from 'marv/api/promise.js'
+import driver from 'marv-mysql-driver'
 import { config } from '../config.js'
-import { getPool } from './pool.js'
-import { execute } from './query.js'
 
 ;(async () => {
-  const db = await getPool(config.db)
-  try {
-    console.log('Dropping table (if exists)...')
-    await execute(db, `
-      DROP TABLE IF EXISTS user;
-    `)
-    console.log('Dropping table (if exists)...DONE.')
-
-    console.log('Creating table...')
-    await execute(db, `
-      CREATE TABLE user (
-        PRIMARY   KEY (id),
-        id        CHAR(36)     NOT NULL,
-        email     VARCHAR(256) NOT NULL,
-        firstname VARCHAR(256) NOT NULL
-      );
-    `)
-    console.log('Creating table...DONE.')
-
-    db.end()
-  } catch (error) {
-    console.error(error.stack)
-    db.end()
-    process.exit(error.code || 1)
-  }
+  const directory = resolve('migrations')
+  const migrations = await marv.scan(directory)
+  await marv.migrate(migrations, driver({ connection: config.db }))
 })()
